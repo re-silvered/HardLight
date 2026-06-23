space-wizards/space-station-14

Admins can now see what items are contraband and chameleon in the strip menu

Content.Client/Strip/StrippingMenu.cs
@@ -9,7 +9,7 @@ namespace Content.Client.Strip
     {
         public LayoutContainer InventoryContainer = new();
         public LayoutContainer HandsContainer = new();
-        public BoxContainer SnareContainer = new();
+        public BoxContainer ButtonCointainer = new() { Orientation = LayoutOrientation.Vertical };
         public bool Dirty = true;
 
         public event Action? OnDirty;
@@ -18,7 +18,7 @@ namespace Content.Client.Strip
         {
             var box = new BoxContainer() { Orientation = LayoutOrientation.Vertical, Margin = new Thickness(0, 8) };
             ContentsContainer.AddChild(box);
-            box.AddChild(SnareContainer);
+            box.AddChild(ButtonCointainer);
             box.AddChild(HandsContainer);
             box.AddChild(InventoryContainer);
         }
@@ -27,7 +27,7 @@ namespace Content.Client.Strip
         {
             InventoryContainer.RemoveAllChildren();
             HandsContainer.RemoveAllChildren();
-            SnareContainer.RemoveAllChildren();
+            ButtonCointainer.RemoveAllChildren();
         }
 
         protected override void FrameUpdate(FrameEventArgs args)

re-silvered/HardLight

(cherry picked from commit 19f18cdb58fe9fce56afd6383765b13c39ffa468)

Content.Client/Strip/StrippingMenu.cs
@@ -8,26 +8,26 @@ namespace Content.Client.Strip
     public sealed class StrippingMenu : DefaultWindow
     {
         public LayoutContainer InventoryContainer = new();
-        public BoxContainer HandsContainer = new() { Orientation = LayoutOrientation.Horizontal };
-        public BoxContainer SnareContainer = new();
+        public LayoutContainer HandsContainer = new();
+        public BoxContainer ButtonContainer = new() { Orientation = LayoutOrientation.Vertical, Margin = new Thickness(0, 0, 0, 5) };
         public bool Dirty = true;
 
         public event Action? OnDirty;
 
         public StrippingMenu()
         {
-            var box = new BoxContainer() { Orientation = LayoutOrientation.Vertical, Margin = new Thickness(0, 8) };
-            Contents.AddChild(box);
-            box.AddChild(SnareContainer);
+            var box = new BoxContainer() { Orientation = LayoutOrientation.Vertical };
+            ContentsContainer.AddChild(box);
+            box.AddChild(ButtonContainer);
             box.AddChild(HandsContainer);
             box.AddChild(InventoryContainer);
         }
 
         public void ClearButtons()
         {
-            InventoryContainer.DisposeAllChildren();
-            HandsContainer.DisposeAllChildren();
-            SnareContainer.DisposeAllChildren();
+            InventoryContainer.RemoveAllChildren();
+            HandsContainer.RemoveAllChildren();
+            ButtonContainer.RemoveAllChildren();
         }
 
         protected override void FrameUpdate(FrameEventArgs args)